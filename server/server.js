518fa3dee513bae175578f0ec10b17813444d39a
Auro <000computricks@gmail.com>
2021-02-02 10:00:07 +0530

    basic server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const { request } = require("express");
+
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.port || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`);
+});
